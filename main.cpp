9e9f0450a164cbc668551eeaf01297ea93a2f5f3
Ben <benwithington@hotmail.com>
2022-06-02 20:30:32 +0100

    glfw setup complete

--- main.cpp
+++ main.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
+#include "GLFW/glfw3.h"
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    GLFWwindow* window;
 
-    return 0;
+    if(!glfwInit())
+    {
+        std::cout << "Failed to Initialise GLFW\n";
+        return EXIT_FAILURE;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+
+    window = glfwCreateWindow(640, 480, "Hello, Dear ImGui!", nullptr, nullptr);
+    if(!window)
+    {
+        std::cout << "Failed to create GLFW window\n";
+        glfwTerminate();
+        return EXIT_FAILURE;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    /* Set window callbacks */
+    glfwSetKeyCallback(window, keyCallback);
+
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return EXIT_SUCCESS;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
